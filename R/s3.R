3e605f47534dd717f151eb0ce21d291f686d84ca
Richie Cotton <richie@datacamp.com>
2021-11-15 20:31:18 -0500

    reinstate create_s3_session(); make s3() safe

--- R/s3.R
+++ R/s3.R
@@ -1,3 +1,46 @@
+#' Create Amazon S3 session
+#' 
+#' Creates an Amazon Simple Storage Service session. (This is where DataCamp  
+#' Data Connector data is hosted).
+#' 
+#' @param access_key_id String containing the S3 access key. 
+#' @param secret_access_key String containing the S3 secret access key.
+#' @param region String containing the S3 default region.
+#' @note Find the arguments to this function in the Admin Portal for
+#' your DataCamp group. See Reporting -> Export -> Data Connector -> View
+#' Configuration Details. It is recommended to store these values as environment 
+#' variables.
+#' @examples 
+#' \dontrun{
+#' Sys.setenv(
+#'   AWS_ACCESS_KEY_ID = "your access key ID",
+#'   AWS_SECRET_ACCESS_KEY = "your secret key",
+#'   AWS_DEFAULT_REGION = "your default region"
+#' )
+#' }
+#' s3_sess <- create_s3_session()
+#' str(s3_sess, 1)
+#' @seealso \code{\link[paws]{s3}}
+#' @importFrom paws s3
+#' @export
+create_s3_session <- function(
+  access_key_id = Sys.getenv("AWS_ACCESS_KEY_ID"), 
+  secret_access_key = Sys.getenv("AWS_SECRET_ACCESS_KEY"), 
+  region = Sys.getenv("AWS_DEFAULT_REGION")) {
+  paws::s3(
+    config = list(
+      credentials = list(
+        creds = list(
+          access_key_id = access_key_id,
+          secret_access_key = secret_access_key
+        )
+      ),
+      region = region
+    )
+  )
+}
+
+
 #' List objects in an S3 bucket
 #'
 #' @param bucket The name of the S3 bucket.
@@ -10,15 +53,15 @@
 #' list_objects_s3() %>%
 #'   head()
 #' }
-list_objects_s3 <- function(bucket = Sys.getenv("AWS_BUCKET"), date = 'latest'){
-  s3 <- paws::s3()
+list_objects_s3 <- function(bucket = Sys.getenv("AWS_S3_BUCKET_NAME"), date = 'latest'){
+  s3 <- create_s3_session()
   # usethis::ui_info("Fetching first 1000 objects ...")
   response <- s3$list_objects_v2(bucket, Prefix = date)
   content <- response$Contents
-  is_truncated <- response$IsTruncated
+  is_truncated <- isTRUE(response$IsTruncated)
   while (is_truncated){
     # usethis::ui_info("Fetching next 1000 objects ...")
-    response <-  s3$list_objects_v2(
+    response <- s3$list_objects_v2(
       bucket,
       Prefix = date,
       ContinuationToken = response$NextContinuationToken
@@ -56,16 +99,25 @@ list_tables_s3 <- function(){
 #' }
 #' @importFrom paws s3
 s3_tbl <- memoise::memoise(function(x, date = 'latest'){
-  s3 <- paws::s3()
+  s3 <- create_s3_session()
   tf <- tempfile(fileext = '.csv')
-  file = glue::glue('{date}/{x}.csv')
-  s3$get_object(
-    Bucket = Sys.getenv("AWS_BUCKET"),
-    Key = glue::glue('{date}/{x}.csv')
-  ) %>%
-    magrittr::extract2('Body') %>%
-    writeBin(con = tf)
-  dc_read_table(tf)
+  file <- glue::glue('{date}/{x}.csv')
+  key <- glue::glue('{date}/{x}.csv')
+  tryCatch(
+    {
+      s3$get_object(
+        Bucket = Sys.getenv("AWS_S3_BUCKET_NAME"),
+        Key = key
+      ) %>%
+        magrittr::extract2('Body') %>%
+        writeBin(con = tf)
+      dc_read_table(tf)
+    },
+    error = function(e) {
+      warning(sprintf("%s could not be read from S3.", key))
+      NULL
+    }
+  )
 })
 
 s3_tbl_docs <- function(){
@@ -95,7 +147,6 @@ s3_help <- function(x){
       tbl = .,
       label = 'Data Connector'
     )
-
 
   informant <- informant %>%
     pointblank::info_tabular(
