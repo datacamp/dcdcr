9213f905c568ae11d3e79abd69d815fc55c548b3
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2021-11-16 06:56:46 -0800

    feat: check for missing env vars

--- R/connector.R
+++ R/connector.R
@@ -5,6 +5,20 @@
 #' @importFrom rlang set_names
 #' @export
 data_connector <- function(date = "latest"){
+  missing_creds <- get_missing_credentials()
+  if (length(missing_creds) > 0){
+    usethis::ui_info("You are missing environment variables")
+    missing_creds %>% 
+      purrr::walk(~ usethis::ui_todo(.x))
+    usethis::ui_stop(paste(
+      "Please add missing environment variables before accessing the data",
+      "connector. If you are in DataCamp Workspace, you can do this using the" ,
+      "Integrations tab in the sidebar on the left. If you are NOT in DataCamp",
+      "Wrkspace, it is recommended that you set these environment variables" ,
+      "in a .Rprofile file at the root of your project. e.g. ",
+      "Sys.setenv(<ENV_VAR> = '...')"
+    ))
+  }
   .tbl <- list_tables_s3() %>%
     map(~ {
       fun <- s3_tbl_binder(.x, date)
