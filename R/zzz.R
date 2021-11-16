c88cd0215e7896ff50f88c14cb268105eee44ceb
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2021-11-16 06:58:03 -0800

    feat: set default env vars on load

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,11 @@
 .onLoad <- function(libname, pkgname) {
+  # Set default environment variables expected by paws::s3()
+  if (Sys.getenv('AWS_BUCKET') == ''){
+    Sys.setenv('AWS_BUCKET' = Sys.getenv('AWS_S3_BUCKET_NAME'))
+  }
+  if (Sys.getenv('AWS_REGION') == ''){
+    Sys.setenv('AWS_REGION' = Sys.getenv('AWS_DEFAULT_REGION'))
+  }
   # Get package environment
   pkg_ns_env <- parent.env(environment())
   # usethis::ui_info("Creating accessor functions ...")
