c82756bb88613280eeee2bc185ffd7ddd22e9db2
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2021-11-22 14:12:50 -0800

    chore: update vignette

--- R/globals.R
+++ R/globals.R
@@ -1,4 +1,4 @@
 globalVariables(c(
   "key", ".", "tbl_fun_name", "tbl_fun_name", "table_description",
-  "column", "description", "table_name"
+  "column", "description", "table_name", "column_name", "column_description"
 ))
